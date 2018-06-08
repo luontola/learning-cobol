4b91357d4976edb5a8836b2cc59cea2bf7b56753
Esko Luontola <esko@luontola.fi>
2018-06-08 22:12:37 +0100

    Draw a blinker

--- game-of-life.cob
+++ game-of-life.cob
@@ -12,11 +12,18 @@
          05 new-rows occurs 10 times.
            10 new-columns occurs 10 times.
              15 pic 9 value 0.
-       01 counter pic 9(2) value 0.
+       01 print-counter pic 9(2) value 0.
        procedure division.
-           perform display-row until counter is equal to 11.
+           move 1 to new-columns(4,5)
+           move 1 to new-columns(5,5)
+           move 1 to new-columns(6,5)
+           perform print-world.
        stop run.
 
+       print-world.
+           move 1 to print-counter.
+           perform display-row until print-counter is equal to 11.
+
        display-row.
-           display new-rows(counter).
-           add 1 to counter.
+           display new-rows(print-counter).
+           add 1 to print-counter.
