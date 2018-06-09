a44d5c94d2699433eaf97b131eb18e51e1d937fa
Esko Luontola <esko@luontola.fi>
2018-06-09 23:02:04 +0100

    Improve test runner to handle process crashes

--- test-game-of-life.sh
+++ test-game-of-life.sh
@@ -1,4 +1,20 @@
-#!/bin/sh
-set -eu
-./run.sh game-of-life.cob | sed 's/\x1B\[2J/----------/' > test-game-of-life.actual
-diff --unified=10 test-game-of-life.expected test-game-of-life.actual | more
+#!/bin/bash
+set -u
+
+OUTPUT=$(./run.sh game-of-life.cob)
+RESULT=$?
+echo "$OUTPUT" | sed 's/\x1B\[2J/----------/' > test-game-of-life.actual
+
+# process crashed, print its output as-is
+if [ $RESULT != 0 ]; then
+    echo -n "$OUTPUT"
+    exit $RESULT
+fi
+
+# compare output against expected
+DIFF=$(diff --unified=10 test-game-of-life.expected test-game-of-life.actual)
+if [ ! -z "$DIFF" ]; then
+    echo "$DIFF" | more
+    exit 1
+fi
+exit 0
